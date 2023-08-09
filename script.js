c3e395e35256bf0be71940782ad29036fb0ecfad
xm-wq <128891971+xm-wq@users.noreply.github.com>
2023-08-09 14:00:36 +0800

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// script.js
 (function () {
-
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
   for (var i = 0; i < names.length; i++) {
@@ -12,5 +10,4 @@
       helloSpeaker.speak(names[i]);
     }
   }
-
 })();
