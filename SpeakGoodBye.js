b78bffd1b7a4f2cb899995d540586d2624e34b96
xm-wq <128891971+xm-wq@users.noreply.github.com>
2023-08-09 13:50:48 +0800

    SpeakGoodBye.js

---
+++ SpeakGoodBye.js
@@ -0,0 +1,14 @@
+// SpeakGoodBye.js
+(function (window) {
+
+  var byeSpeaker = {};
+
+  var speakWord = "Good Bye";
+
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.byeSpeaker = byeSpeaker;
+
+})(window);
